6cd12aac81380fa4760fd603c5d46b38d99f38da
Victor1305M <111284915+Victor1305M@users.noreply.github.com>
2022-09-01 11:21:04 -0300

    Add files via upload

---
+++ Dev/programação c 2.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h> //Biblioteca de entrada/saída por periféricos padrão
+#include<conio.h> //Biblioteca para manipulação de caracteres
+#include<stdlib.h>//Biblioteca padrão para uso de funções do sistema
+int main () {//Programa principal
+     //Declaração de Variáveis
+     int marcaA, marcaB, marcaC; // Variáveis do tipo inteiro para marca A, B e C
+     //Cabeçalho
+      printf("Universidade de Bras%lia\n",161);
+    printf("Disciplina: 113913 - ICC \n");
+    printf("Aluno (a) :\n\n\n");
+    /* Os seguintes comandos printf são para imprimir a mensagem dentro das "" para que haja
+    comunicação entre computador e o usúario do programa.As funções scanf sevem para receber
+    os números desejados,o "%d" indica que a variável ao qual será destinada o valor recebido 
+    é do tipo inteiro.o & indica que será armazenado no espaço declarao anteriormente para va
+    riavel que vem logo em seguida deste &. */
+         printf("Digite a quantidade de chuteiras de marca A:");
+         scanf("%d",&marcaA);
+          printf("Digite a quantidade de chuteiras de marca B:");
+         scanf("%d",&marcaB);
+          printf("Digite a quantidade de chuteiras de marca C:");
+         scanf("%d",&marcaC);
+         
+         //Apresentação dos Resultados Letra A)
+         printf("\nExistem %d chuteiras da marca A\n", marcaA);
+         print("Existem %d")
+         
